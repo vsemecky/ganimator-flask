71f4e27df0faadf333f667cafaded1084bef20f8
Vojtech Semecky <vojtech@semecky.cz>
2020-11-05 20:37:57 +0100

    Debug

--- project.py
+++ project.py
@@ -9,15 +9,17 @@ class Project:
     psi = 0.75
 
     def __init__(self, data_dir):
+        os.makedirs(data_dir, exist_ok=True)
         self.data_dir = data_dir
         self.image_seeds = self.get_image_seeds()
         self.style_seeds = self.get_style_seeds()
         self.pkl = self.get_pkl_filename()
-        os.makedirs(data_dir, exist_ok=True)
 
     def get_pkl_filename(self):
         pkls = glob.glob(self.data_dir + '/*.pkl')
+        print("Pkl files found:")
         pprint(pkls)
+        print("Using pkl:", pkls[0])
         return pkls[0]
 
     def get_seed_url(self, seed):
